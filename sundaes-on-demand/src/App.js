be45714aec162e3c961af89f76a02979d147aad3
onelky <onelky.hernandez29@gmail.com>
2023-09-28 16:28:40 -0400

    install Bootstrap library

--- sundaes-on-demand/src/App.js
+++ sundaes-on-demand/src/App.js
@@ -1,5 +1,13 @@
+import { Button } from 'react-bootstrap'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Button as="a" variant="secondary">
+        Button as link
+      </Button>
+    </div>
+  )
 }
 
 export default App
