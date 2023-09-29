1afb40f3821f04fc684f9e57fc8a02e25e5671fb
onelky <onelky.hernandez29@gmail.com>
2023-09-28 20:48:29 -0400

    add and test popup in SummaryForm

--- sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
+
+const popover = (
+  <Popover>
+    <Popover.Body>Nothing will be delivered</Popover.Body>
+  </Popover>
+)
 
 const SummaryForm = () => {
   const [checked, setChecked] = useState(false)
@@ -9,13 +17,15 @@ const SummaryForm = () => {
   return (
     <div>
       <Form>
-        <Form.Check
-          id={'agree-to-terms'}
-          type="checkbox"
-          label="I agree to Terms and Conditions"
-          onClick={handleClick}
-          checked={checked}
-        />
+        <Form.Check id={'agree-to-terms'} type="checkbox">
+          <Form.Check.Input type={'checkbox'} onClick={handleClick} checked={checked} />
+          <Form.Check.Label>
+            I agree to
+            <OverlayTrigger trigger={'hover'} placement={'right'} overlay={popover}>
+              <span style={{ color: 'blue' }}>Terms and Conditions</span>
+            </OverlayTrigger>
+          </Form.Check.Label>
+        </Form.Check>
       </Form>
       <Button disabled={!checked}>Confirm order</Button>
     </div>
--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import SummaryForm from '../SummaryForm'
 
 describe('Summary Form', () => {
@@ -10,15 +11,38 @@ describe('Summary Form', () => {
     const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' })
     expect(checkbox).not.toBeChecked()
   })
-  test('Checkbox enables submit button when checked, and disables it when unchecked', () => {
+  test('Checkbox enables submit button when checked, and disables it when unchecked', async () => {
+    const user = userEvent.setup()
     render(<SummaryForm />)
+
     const button = screen.getByRole('button', { name: 'Confirm order' })
     const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' })
 
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(button).toBeEnabled()
 
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(button).toBeDisabled()
+  })
+
+  test('Popup is shown when "terms and conditions" are hovered and dissappears when unhover', async () => {
+    const user = userEvent.setup()
+    const popoverText = /nothing will be delivered/i
+    render(<SummaryForm />)
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i)
+    expect(termsAndConditions).toHaveStyle({ color: 'blue' })
+
+    // popover is not initially shown
+    expect(screen.queryByText(popoverText)).not.toBeInTheDocument()
+
+    // popover appears on hover
+    await user.hover(termsAndConditions)
+    const popover = screen.getByText(popoverText)
+    expect(popover).toBeInTheDocument()
+
+    // popover disappears on mouse leave
+    await user.unhover(termsAndConditions)
+    expect(popover).not.toBeInTheDocument()
   })
 })
