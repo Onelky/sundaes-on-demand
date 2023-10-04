005581938c31b1a046ab91c2f20a78d5e43203df
onelky <onelky.hernandez29@gmail.com>
2023-10-04 19:36:53 -0400

    handle and test errors in Options

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
+import Alert from 'react-bootstrap/Alert'
 import ScoopOption from './ScoopOption.jsx'
 import ToppingOption from './ToppingOption'
 import { BACKEND_BASE_URL } from '../../mocks/handlers'
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([])
+  const [error, setError] = useState('')
 
   useEffect(() => {
     axios
       .get(BACKEND_BASE_URL + optionType)
       .then((result) => setItems(result.data))
-      .catch((e) => console.error(e))
+      .catch(({ response: { data } }) => setError(data.message))
   }, [optionType])
 
   const Item = optionType === 'scoops' ? ScoopOption : ToppingOption
 
   return (
     <div>
-      {items.map((item) => (
-        <div key={item.id ?? item.name} style={{ display: 'flex', flexDirection: 'column' }}>
-          <Item key={item.name} item={item} />
-        </div>
-      ))}
+      {error ? (
+        <Alert>{error}</Alert>
+      ) : (
+        items.map((item) => (
+          <div key={item.id ?? item.name} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Item key={item.name} item={item} />
+          </div>
+        ))
+      )}
     </div>
   )
 }
--- sundaes-on-demand/src/pages/entry/test/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -2,9 +2,6 @@ import { render, screen } from '@testing-library/react'
 import Options from '../Options'
 
 describe('Options Component', () => {
-  test('Initial State: Total is zero', () => {
-    // render(<Options />)
-  })
   test('Displays image for each scoop option from the server', async () => {
     render(<Options optionType={'scoops'} />)
     const images = await screen.findAllByRole('img', { name: /scoop$/i })
@@ -21,11 +18,5 @@ describe('Options Component', () => {
 
     const altTexts = images.map((image) => image.alt)
     expect(altTexts).toEqual(['M&Ms topping', 'Hot fudge topping'])
-  })
-  test('Option amount increases when + button is clicked', () => {
-    // render(<Options />)
-  })
-  test('Option amount is reduced when - button is clicked', () => {
-    // render(<Options />)
   })
 })
